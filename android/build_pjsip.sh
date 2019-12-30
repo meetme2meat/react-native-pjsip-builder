f3e87ab26112b5d89563bbebb91f60e3ac7bbea7
Viren Negi <meetme2meat@users.noreply.github.com>
2019-12-30 15:59:36 +0530

    Disable video

--- android/build_pjsip.sh
+++ android/build_pjsip.sh
@@ -14,7 +14,7 @@ cat <<EOF > "/tmp/pjsip/pjlib/include/pj/config_site.h"
 #define PJSIP_INCLUDE_ALLOW_HDR_IN_DLG 0
 #define PJMEDIA_HAS_G7221_CODEC 1
 #include <pj/config_site_sample.h>
-#define PJMEDIA_HAS_VIDEO 1
+#define PJMEDIA_HAS_VIDEO 9
 #define PJMEDIA_AUDIO_DEV_HAS_ANDROID_JNI 0
 #define PJMEDIA_AUDIO_DEV_HAS_OPENSL 1
 #define PJSIP_AUTH_AUTO_SEND_NEXT 0
